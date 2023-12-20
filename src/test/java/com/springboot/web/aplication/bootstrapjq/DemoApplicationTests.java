110e7a8c70cb5061ab932750ec3d5cf373f3e38b
SheikhMuz <137673355+SheikhMuz@users.noreply.github.com>
2023-12-20 23:50:50 +0530

    Add files via upload

---
+++ src/test/java/com/springboot/web/aplication/bootstrapjq/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.web.aplication.bootstrapjq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
